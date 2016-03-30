3b23fb298fd7a8a6b18c04eea16d2e2bddeb243a
Maladrumthrasky <malpower@ymail.com>
2016-03-31 01:29:28 +0800

    Dialog api added

---
+++ bin/common.js
@@ -0,0 +1,30 @@
+var sys=new Object;
+var dialogs;
+var fs=require("fs");
+$(document).ready(function()
+{//init here.
+    fs.readFile("Thrastep/dialogs.html",function(err,html)
+    {
+        dialogs=$(html.toString());
+        sys.loadDialog=function(cls,options,callback)
+        {
+            options=options || {};
+            callback=callback || function(){};
+            var html=dialogs.find("."+cls).html();
+            html="<div class=\""+cls+"\">"+html+"</div>";
+            options.content=html;
+            options.type=1;
+            options.closeBtn=options.closeBtn || 2;
+            var winId=layer.open(options);
+            var dom=$("layui-layer"+winId).find("."+cls);
+            process.nextTick(function()
+            {
+                callback(dom,winId);
+            });
+        };
+
+
+
+        Startup();
+    });
+});
--- bin/mod_entry.js
+++ bin/mod_entry.js
@@ -4,40 +4,79 @@ function InitMenu()
     var top=new nw.Menu({type: "menubar"});
     var menu=new nw.Menu;
     top.append(new nw.MenuItem({label: "File",submenu: menu}));
-    var m=nw.MenuItem({label: "Open",key: "o",modifiers: "ctrl"});
+    var m=new nw.MenuItem({label: "Open",key: "o",modifiers: "ctrl"});
     menulib["file-open"]=m;
     menu.append(m);
-    m=nw.MenuItem({label: "Save",key: "s",modifiers: "ctrl"});
+    m=new nw.MenuItem({label: "Save",key: "s",modifiers: "ctrl"});
     menulib["file-save"]=m;
     menu.append(m);
-    m=nw.MenuItem({label: "Save As",key: "s",modifiers: "ctrl+shift"});
+    m=new nw.MenuItem({label: "Save As",key: "s",modifiers: "ctrl+shift"});
     menulib["file-saveas"]=m;
     menu.append(m);
-    m=nw.MenuItem({type: "separator"});
+    m=new nw.MenuItem({type: "separator"});
     menu.append(m);
-    m=nw.MenuItem({label: "Quit",key: "q",modifiers: "ctrl"});
+    m=new nw.MenuItem({label: "Quit",key: "q",modifiers: "ctrl"});
     menulib["file-quit"]=m;
     menu.append(m);
     menu=new nw.Menu;
     top.append(new nw.MenuItem({label: "Edit",submenu: menu}));
-    m=nw.MenuItem({label: "Undo",key: "z",modifiers: "ctrl"});
-    menulib["file-undo"]=m;
+    m=new nw.MenuItem({label: "Undo",key: "z",modifiers: "ctrl"});
+    menulib["edit-undo"]=m;
     menu.append(m);
-    m=nw.MenuItem({label: "Redo",key: "y",modifiers: "ctrl"});
-    menulib["file-redo"]=m;
+    m=new nw.MenuItem({label: "Redo",key: "y",modifiers: "ctrl"});
+    menulib["edit-redo"]=m;
     menu.append(m);
-    m=nw.MenuItem({type: "separator"});
+    m=new nw.MenuItem({type: "separator"});
     menu.append(m);
-    m=nw.MenuItem({label: "Preferences"});
-    menulib["file-preferences"]=m;
+    m=new nw.MenuItem({label: "Preferences"});
+    menulib["edit-preferences"]=m;
     menu.append(m);
     menu=new nw.Menu;
     top.append(new nw.MenuItem({label: "Analysis",submenu: menu}));
-    m=nw.MenuItem({label: "PE Header"});
-    menulib["file-peheader"]=m;
+    m=new nw.MenuItem({label: "PE Header"});
+    menulib["analysis-peheader"]=m;
     menu.append(m);
-    m=nw.MenuItem({label: "ELF Header"});
-    menulib["file-elfheader"]=m;
+    m=new nw.MenuItem({label: "ELF Header"});
+    menulib["analysis-elfheader"]=m;
+    menu.append(m);
+    menu=new nw.Menu;
+    top.append(new nw.MenuItem({label: "Search",submenu: menu}));
+    m=new nw.MenuItem({label: "Go To Address",key: "g",modifiers: "ctrl"});
+    menulib["search-gotoaddress"]=m;
+    menu.append(m);
+    m=new nw.MenuItem({label: "Search Text",key: "f",modifiers: "ctrl"});
+    menulib["search-searchtext"]=m;
+    menu.append(m);
+    m=new nw.MenuItem({label: "Search Hex",key: "f",modifiers: "ctrl+shift"});
+    menulib["search-searchhex"]=m;
+    menu.append(m);
+    menu=new nw.Menu;
+    top.append(new nw.MenuItem({label: "Tools",submenu: menu}));
+    m=new nw.MenuItem({label: "Scale"});
+    menulib["tools-scale"]=m;
+    menu.append(m);
+    menu=new nw.Menu;
+    top.append(new nw.MenuItem({label: "Help",submenu: menu}));
+    m=new nw.MenuItem({label: "View License"});
+    menulib["help-viewlicense"]=m;
+    menu.append(m);
+    m=new nw.MenuItem({label: "View Developers"});
+    menulib["help-viewdevelopers"]=m;
+    menu.append(m);
+    m=new nw.MenuItem({type: "separator"});
+    menu.append(m);
+    m=new nw.MenuItem({label: "Documents"});
+    menulib["help-documents"]=m;
+    menu.append(m);
+    m=new nw.MenuItem({type: "separator"});
+    menu.append(m);
+    m=new nw.MenuItem({label: "About Thrastep"});
+    menulib["help-aboutthrastep"]=m;
+    menu.append(m);
+    m=new nw.MenuItem({type: "separator"});
+    menu.append(m);
+    m=new nw.MenuItem({label: "Welcome Guide"});
+    menulib["help-welcomeguide"]=m;
     menu.append(m);
     return top;
 }
@@ -48,9 +87,13 @@ function InitMenu()
 
 
 
-$(document).ready(function()
+function Startup()
 {
     var win=nw.Window.get();
     win.showDevTools();
     win.menu=InitMenu();
-});
+    menulib["help-aboutthrastep"].click=function()
+    {
+        sys.loadDialog("dlgAbout",{area: ["500px","300px"],title: "About Thrastep"});
+    };
+}
