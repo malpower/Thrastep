689917ee9776602248b841cbb0c6cb1f5ded0dee
Maladrumthrasky <malpower@ymail.com>
2016-03-31 23:18:35 +0800

    Trying to add open file feature

---
+++ coms/tfile.js
@@ -0,0 +1,28 @@
+var fs=require("fs");
+function TFile(path)
+{
+    var buffer=new Buffer(0);
+    this.open=function()
+    {
+        var that=this;
+        this.status="OPENING";
+        var rs=fs.createReadStream(path);
+        rs.on("data",function(chunk)
+        {
+            buffer=Buffer.concat([buffer,chunk]);
+        }).on("end",function()
+        {
+            that.content=buffer;
+            that.length=buffer.length;
+            that.status="OPENED";
+            that.onopen();
+        });
+    };
+    this.onopen=function(){};
+    this.content=buffer;
+    this.path=path;
+    this.status="NOT OPEN";
+}
+
+
+module.exports=TFile;
